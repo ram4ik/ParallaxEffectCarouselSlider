229b3981dd917e50d284d312cdfd10cd63c0de5e
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-12-03 21:59:19 +0200

    Initial Commit

---
+++ ParallaxEffectCarouselSlider/ParallaxEffectCarouselSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxEffectCarouselSliderApp.swift
+//  ParallaxEffectCarouselSlider
+//
+//  Created by ramil on 03.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxEffectCarouselSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
